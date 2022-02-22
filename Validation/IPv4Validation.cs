6ec2707e616146ff8b37ffe2e98a7a05eb6f764c
KlaasHagge <klaash@ppedv.de>
2022-02-22 14:17:52 +0100

    Validation

--- Validation/IPv4Validation.cs
+++ Validation/IPv4Validation.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Markup;
 
 namespace Validation
 {
@@ -22,6 +25,28 @@ namespace Validation
             else
                 //Andernfalls wird eine Fehlermeldung zurückgegeben
                 return new ValidationResult(false, "Bitte gib eine IPv4-Adresse ein.");
+        }
+    }
+
+    public class ValidationRuleCollection : Collection<ValidationRule> { }
+
+    public class ValidationMarkup : MarkupExtension
+    {
+        private Binding binding;
+
+        public ValidationMarkup(Binding binding, ValidationRuleCollection validationRules)
+        {
+            this.binding = binding;
+
+            foreach (var item in validationRules)
+            {
+                binding.ValidationRules.Add(item);
+            }
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return binding.ProvideValue(serviceProvider);
         }
     }
 }
